dbc0a04f3cd8baeb2b36a87d8d56f71832347cb9
blankman2k6-spec <blankman2k6@yahoo.com>
2026-08-06 20:48:15 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "ab-rfq-v2";
+const CACHE = "ab-rfq-v3";
 const ASSETS = ["./","index.html","manifest.json","icon-192.png","icon-512.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
