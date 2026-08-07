bff23659387700d766001dea9dd60438365c9cf3
blankman2k6-spec <blankman2k6@yahoo.com>
2026-08-07 02:46:23 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="ab-sales-os-v3-3-ui-polish";
+const CACHE="ab-sales-os-v3-4-monthly-customer-log";
 const ASSETS=["./","index.html","manifest.json","icon-192.png","icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
