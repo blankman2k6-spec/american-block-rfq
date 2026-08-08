98fd7dd3f84688e2e122d6f8aa03db4b386fa2b8
blankman2k6-spec <blankman2k6@yahoo.com>
2026-08-08 02:47:19 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="ab-sales-os-v4-6-5-backorders";
+const CACHE="ab-sales-os-v4-6-6-territory-review";
 const ASSETS=["./","index.html","manifest.json","icon-192.png","icon-512.png"];
 
 self.addEventListener("install",event=>{
