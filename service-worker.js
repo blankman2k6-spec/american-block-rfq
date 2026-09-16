f787c2a2f90b70595cb41572f376d5c3254463d8
blankman2k6-spec <blankman2k6@yahoo.com>
2026-09-16 17:44:15 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="ab-sales-os-v5-7-5-image-import-orders-rfqs";
+const CACHE="ab-sales-os-v5-8-0-quick-document-builder";
 const ASSETS=["./","index.html","manifest.json","icon-192.png","icon-512.png","assets/ABCO-FLOWLINE-CATALOG.pdf","ABCO-FLOWLINE-CATALOG.pdf"];
 
 self.addEventListener("install",event=>{
